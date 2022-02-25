5a1fc7e0167be684a067baae3fc761e7a222e437
Nidhi Verma <vnidhi235@gmail.com>
2022-02-25 19:43:15 +0530

    Counter App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from "react";
+
+const App = () => {
+
+  const [counter, setCounter] = useState(0);
+  const [userCount, getUserCount] = useState(0);
+  //Increment method
+  const handleIncrement = () => {
+    setCounter(counter + userCount);
+  }
+
+  //Decrement method
+  const handleDecrement = () => {
+    setCounter(counter - userCount);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+
+      <h2 style={{
+        textAlign: 'center'
+      }}>Check this counter App!</h2>
+      <div style={{
+        fontSize: '120%',
+        position: 'relative',
+        top: '10vh',
+      }}>
+        <div style={{
+          textAlign: 'center'
+        }}>
+          <h5 >Set the counter value?</h5>
+
+          <input name='getCounter' onChange={event => getUserCount(Number.parseInt(event.target.value))}></input>
+          <button>Set counter</button>
+        </div>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'row',
+          alignItems: 'center',
+          fontSize: '200%',
+          marginLeft: '35%'
+
+
+        }}>
+          <button style={{
+            fontSize: '50%',
+            position: 'relative',
+            top: '20vh',
+            marginRight: '5px',
+            backgroundColor: '#278021',
+            color: 'white',
+
+
+          }}
+            onClick={handleIncrement} title="I will increase the value">Increment</button>
+          <div style={{
+            backgroundColor: '#212010',
+            height: '80px',
+            width: '120px',
+            textAlign: 'center',
+            paddingBottom: '2%',
+            color: 'white'
+
+          }}
+          >{counter}</div>
+          <button style={{
+            fontSize: '50%',
+            position: 'relative',
+            top: '20vh',
+            marginLeft: '5px',
+            backgroundColor: 'red',
+            color: 'white',
+          }}
+            onClick={handleDecrement} title="I will decrease the value">Decrement</button>
+        </div>
+        <div >
+
+        </div>
+      </div>
+
+    </>
+  )
 }
+
 
 export default App;
